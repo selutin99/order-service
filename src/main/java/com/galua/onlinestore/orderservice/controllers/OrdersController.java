846a1d682e7fda13e2441080450dc8ba23eb051f
Alexander <cool.selutin99@yandex.ru>
2019-04-26 20:54:15 +0400

    order controller

--- src/main/java/com/galua/onlinestore/orderservice/controllers/OrdersController.java
+++ src/main/java/com/galua/onlinestore/orderservice/controllers/OrdersController.java
@@ -1,46 +1,70 @@
 package com.galua.onlinestore.orderservice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.galua.onlinestore.orderservice.entities.Orders;
+import com.galua.onlinestore.orderservice.services.OrdersService;
+import com.galua.onlinestore.orderservice.services.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 public class OrdersController {
-    /*@Autowired
+    @Autowired
+    private OrdersService ordersService;
+
+    @Autowired
     private StatusService statusService;
 
-    @GetMapping("status/{id}")
-    public ResponseEntity<Status> getStatusById(@PathVariable("id") int id) {
-        Status status = statusService.getStatusByID(id);
-        return new ResponseEntity<>(status, HttpStatus.OK);
+    @GetMapping("order/{id}")
+    public ResponseEntity<Orders> getOrderById(@PathVariable("id") int id) {
+        Orders order = ordersService.getOrderByID(id);
+        return new ResponseEntity<>(order, HttpStatus.OK);
     }
 
-    @GetMapping("statuses")
-    public ResponseEntity<List<Status>> getAllStatuses() {
-        List<Status> list = statusService.getAllStatuses();
+    @GetMapping("orders")
+    public ResponseEntity<List<Orders>> getAllOrders() {
+        List<Orders> list = ordersService.getAllOrders();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @PostMapping("status")
-    public ResponseEntity<Void> addStatus(@RequestBody Status status, UriComponentsBuilder builder) {
+    @PostMapping("order")
+    public ResponseEntity<Void> addOrder(@RequestBody Orders order, UriComponentsBuilder builder) {
         try {
-            statusService.createStatus(status);
+            ordersService.createOrder(order);
         }
         catch(IllegalArgumentException e){
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/status/{id}").buildAndExpand(status.getId()).toUri());
+        headers.setLocation(builder.path("/order/{id}").buildAndExpand(order.getId()).toUri());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-    @PutMapping("status")
-    public ResponseEntity<Status> updateStatus(@RequestBody Status status) {
-        statusService.updateStatus(status);
-        return new ResponseEntity<>(status, HttpStatus.OK);
+    @PutMapping("order")
+    public ResponseEntity<Orders> updateOrder(@RequestBody Orders order) {
+        ordersService.updateOrder(order);
+        return new ResponseEntity<>(order, HttpStatus.OK);
     }
 
-    @DeleteMapping("status/{id}")
-    public ResponseEntity<Void> deleteStatus(@PathVariable("id") int id) {
-        statusService.deleteStatus(id);
+    @PatchMapping("order/{id}")
+    public ResponseEntity<Void> updateStatus(@RequestBody Orders order,
+                                             @PathVariable("id") int id) {
+        if(order==null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        order.setStatus(statusService.getStatusByID(id));
+        ordersService.updateOrder(order);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("order/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable("id") int id) {
+        ordersService.deleteOrder(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }*/
+    }
 }
