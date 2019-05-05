a1d2c81d43721f88b250a3962545ca6ba06b9062
Alexander <cool.selutin99@yandex.ru>
2019-05-05 13:56:32 +0400

    find dependencies

--- src/main/java/com/galua/onlinestore/orderservice/OrderServiceApplication.java
+++ src/main/java/com/galua/onlinestore/orderservice/OrderServiceApplication.java
@@ -2,8 +2,11 @@ package com.galua.onlinestore.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = { "com.galua.onlinestore.orderservice.entities",
+							 "com.galua.onlinestore.offerservice.entities" })
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
